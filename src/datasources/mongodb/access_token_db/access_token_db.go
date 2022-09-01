4d892345f4a9c0764ca5349d60e42412339227a1
Rafa Williner <rafa.williner@gmail.com>
2022-09-01 16:28:22 -0300

    Fix security issue.

--- src/datasources/mongodb/access_token_db/access_token_db.go
+++ src/datasources/mongodb/access_token_db/access_token_db.go
@@ -2,16 +2,27 @@ package access_token_db
 
 import (
 	"context"
+	"fmt"
+	"os"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var DB *mongo.Client = ConnectDB()
+const (
+	mongo_user_name = "mongo_user_name"
+	mongo_password = "mongo_password"
+)
+
+var (
+	DB *mongo.Client = ConnectDB()
+	username = os.Getenv(mongo_user_name)
+	password = os.Getenv(mongo_password)
+)
 
 func ConnectDB() *mongo.Client  {
-    client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://tokenalertapp:mongodbmamincho@cluster0.i1plsoa.mongodb.net/?retryWrites=true&w=majority"))
+    client, err := mongo.NewClient(options.Client().ApplyURI(fmt.Sprintf("mongodb+srv://%s:%s@cluster0.i1plsoa.mongodb.net/?retryWrites=true&w=majority", username, password)))
     if err != nil {
         panic(err)
     }
